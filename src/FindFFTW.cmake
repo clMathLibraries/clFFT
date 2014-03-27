0db6e07afbf2b1cafed401e6b9d46ab2d503f22f
Kent Knox <kent.knox@amd>
2014-03-27 14:50:38 -0500

    Improved detection of the OpenCL dependencies during the 'copy' step A little refactoring of the FindOpenCL & FindFFTW to make it more standard

--- src/FindFFTW.cmake
+++ src/FindFFTW.cmake
@@ -33,7 +33,9 @@
 #   FFTW_ROOT - (as a CMake or environment variable)
 #                The root directory of the fftw install prefix
 #
-#-----------------------
+#   FIND_LIBRARY_USE_LIB64_PATHS - Global property that controls whether 
+#               findFFTW should search for 64bit or 32bit libs
+#-----------------------------------------------
 # Example Usage:
 #
 #    find_package(FFTW REQUIRED)
@@ -42,53 +44,52 @@
 #    add_executable(foo foo.cc)
 #    target_link_libraries(foo ${FFTW_LIBRARIES})
 #
-#-----------------------
-if( DEFINED ENV{FFTW_ROOT} )
-	set( FFTW_ROOT $ENV{FFTW_ROOT} CACHE PATH "Environment variable defining the root of FFTW" )
-else( )
-	set( FFTW_ROOT "/usr/lib" CACHE PATH "Environment variable defining the root of FFTW" )
-endif( )
+#-----------------------------------------------
 
 find_path(FFTW_INCLUDE_DIRS
-	NAMES fftw3.h
+    NAMES fftw3.h
     HINTS
-        ${FFTW_ROOT}/api
         ${FFTW_ROOT}/include
+        ${FFTW_ROOT}/api
         ${FFTW_ROOT}
-        $ENV{FFTW_ROOT}/api
         $ENV{FFTW_ROOT}/include
-        $ENV{FFTW_ROOT}
-	PATHS
-		/usr/include
-		/usr/local/include
+        $ENV{FFTW_ROOT}/api
+        ENV FFTW_ROOT
+    PATHS
+        /usr/include
+        /usr/local/include
 )
 mark_as_advanced( FFTW_INCLUDE_DIRS )
 
 find_library( FFTW_SINGLE_PRECISION_LIBRARIES
-	NAMES fftw3f libfftw3f-3
-	HINTS
-		${FFTW_ROOT}
-		${FFTW_ROOT}/lib
-		$ENV{FFTW_ROOT}
-		$ENV{FFTW_ROOT}/lib
-	PATHS
-		/usr/lib
-		/usr/local/lib
-	DOC "FFTW dynamic library"
+    NAMES fftw3f libfftw3f-3
+    HINTS
+        ${FFTW_ROOT}/lib
+        ${FFTW_ROOT}/.libs
+        ${FFTW_ROOT}
+        $ENV{FFTW_ROOT}/lib
+        $ENV{FFTW_ROOT}/.libs
+        ENV FFTW_ROOT
+    PATHS
+        /usr/lib
+        /usr/local/lib
+    DOC "FFTW dynamic library"
 )
 mark_as_advanced( FFTW_SINGLE_PRECISION_LIBRARIES )
 
 find_library( FFTW_DOUBLE_PRECISION_LIBRARIES
-	NAMES fftw3 libfftw3-3
-	HINTS
-		${FFTW_ROOT}
-		${FFTW_ROOT}/lib
-		$ENV{FFTW_ROOT}
-		$ENV{FFTW_ROOT}/lib
-	PATHS
-		/usr/lib
-		/usr/local/lib
-	DOC "FFTW dynamic library"
+    NAMES fftw3 libfftw3-3
+    HINTS
+        ${FFTW_ROOT}/lib
+        ${FFTW_ROOT}/.libs
+        ${FFTW_ROOT}
+        $ENV{FFTW_ROOT}/lib
+        $ENV{FFTW_ROOT}/.libs
+        ENV FFTW_ROOT
+    PATHS
+        /usr/lib
+        /usr/local/lib
+    DOC "FFTW dynamic library"
 )
 mark_as_advanced( FFTW_DOUBLE_PRECISION_LIBRARIES )
 
@@ -99,6 +100,6 @@ include( FindPackageHandleStandardArgs )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDE_DIRS )
 
 if( NOT FFTW_FOUND )
-	message( STATUS "FindFFTW looked for single precision libraries named: fftw3f or libfftw3f-3" )
-	message( STATUS "FindFFTW looked for double precision libraries named: fftw3 or libfftw3-3" )
+    message( STATUS "FindFFTW looked for single precision libraries named: fftw3f or libfftw3f-3" )
+    message( STATUS "FindFFTW looked for double precision libraries named: fftw3 or libfftw3-3" )
 endif()
