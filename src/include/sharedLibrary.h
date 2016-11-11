afa9a2af575bbc2cefef3afc518114669a68d399
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-11-11 17:53:18 +0000

    Revert "Enable support for kFreeBSD."
    This reverts commit 29226258e596784e4ce27fed9871013c3322fe1d.

--- src/include/sharedLibrary.h
+++ src/include/sharedLibrary.h
@@ -52,7 +52,7 @@ inline void* LoadSharedLibrary( std::string unixPrefix, std::string libraryName,
   {
           std::cerr << ::dlerror( ) << std::endl;
   }
-#elif defined(__FreeBSD_kernel__)
+#elif defined(__FreeBSD__)
         tstring freebsdName = unixPrefix;
         freebsdName += libraryName += ".so";
         void* fileHandle = ::dlopen( freebsdName.c_str( ), RTLD_NOW );
