ce9460afdefbde694de7d1004a00ffe287ae390d
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-11-11 17:58:18 +0000

    Add support for GNU Hurd and kFreeBSD.

--- src/include/sharedLibrary.h
+++ src/include/sharedLibrary.h
@@ -36,7 +36,7 @@ inline void* LoadSharedLibrary( std::string unixPrefix, std::string libraryName,
 
 	//	HMODULE is actually the load address; function returns NULL if it cannot find the shared library
 	HMODULE fileHandle	= ::LoadLibraryExA( libraryName.c_str( ), NULL, NULL );
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__GNU__) || (defined(__FreeBSD_kernel__) && defined(__GLIBC__))
         tstring linuxName = unixPrefix;
 	linuxName += libraryName += ".so";
 	void* fileHandle = ::dlopen( linuxName.c_str( ), RTLD_NOW );
